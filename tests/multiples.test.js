6cde45b06b9860352fe90c83281d8c95d3204d36
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-08 16:47:10 -0700

    passed test for multiples of 5, had to edit for loop length

--- tests/multiples.test.js
+++ tests/multiples.test.js
@@ -7,6 +7,12 @@ describe('multiple function', () => {
     expect(array).toEqual(expect.any(Array));
   });
 
+  it('returns multiples of 5', () => {
+    const n = 5;
+    const array = multiples(n);
+    expect(array).toEqual([5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]);
+  });
+
   it('returns multiples of 15', () => {
     const n = 15;
     const array = multiples(n);
