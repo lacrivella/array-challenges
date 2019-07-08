f6d4727606ebe99e01258ba9f4a32fc6500a5870
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-08 16:42:38 -0700

    passed 15 test

--- tests/multiples.test.js
+++ tests/multiples.test.js
@@ -2,8 +2,13 @@ const { multiples } = require('../lib/multiples')
 
 describe('multiple function', () => {
   it('returns an array from 1 to 100', () => {
-    const multiple = 1;
-    const array = multiples(multiple);
+    const n = 1;
+    const array = multiples(n);
     expect(array).toEqual(expect.any(Array));
+  });
+  it('returns multiples of 15', () => {
+    const n = 15;
+    const array = multiples(n);
+    expect(array).toEqual([15, 30, 45, 60, 75, 90]);
   });
 });
